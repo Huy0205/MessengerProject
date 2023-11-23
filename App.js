b56e4a2bc77f72426d734fd2d47344fea99436ba
LAPTOP-3BEOF0QJ\PC <20068791.huy@student.iuh.edu.vn>
2023-11-23 15:03:01 +0700

    modified UI Login, SignUp and Start

--- App.js
+++ App.js
@@ -10,15 +10,6 @@ import profile from "./screen/profile";
 import setting from "./screen/setting";
 import editProfile from "./screen/editProfile";
 
-<<<<<<< HEAD
-import Screen1  from './screen1';
-import Screen2  from './screen2';
-import Screen3  from './screen3';
-import profile from './profile';
-import setting from './setting';
-import editProfile from './editProfile';
-=======
->>>>>>> 890f87b (modified UI Login, SignUp and Start)
 const BottomTab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
@@ -26,21 +17,6 @@ export default function App() {
   return (
     // <Start/>
     <NavigationContainer>
-<<<<<<< HEAD
-
-<Stack.Navigator initalRouteName="Home"
-     screenOptions={{headerShown:false}}
->
-<Stack.Screen name="Screen1" component={Screen1}/> 
-<Stack.Screen name="Screen2" component={Screen2}/>
-<Stack.Screen name="Screen3" component={Screen3}/>
-
-<Stack.Screen name="Profile" component={profile}/>
-<Stack.Screen name="Setting" component={setting}/>
-<Stack.Screen name="EditProfile" component={editProfile}/>
-</Stack.Navigator>
-
-=======
       <Stack.Navigator initalRouteName="Login">
         <Stack.Screen
           name="Login"
@@ -66,7 +42,6 @@ export default function App() {
         <Stack.Screen name="Setting" component={setting} />
         <Stack.Screen name="EditProfile" component={editProfile} />
       </Stack.Navigator>
->>>>>>> 890f87b (modified UI Login, SignUp and Start)
     </NavigationContainer>
   );
 }
